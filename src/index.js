4bfd07a99c9032a5543e28d78ab273a5a413a10f
Ayo Solomon <48213053+Ayoseun@users.noreply.github.com>
2024-01-04 10:46:58 +0100

    .

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ const app = express();
 import testnetRoutes from './router/routes.js';
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5800');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 app.use('/api/testnet', testnetRoutes);
 
